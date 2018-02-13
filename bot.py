9dc293093bbc7058f3aca7524cd977fc1050915f
Alexander Myltsev <a.myltsev@gmail.com>
2018-02-13 15:51:58 +0300

    Test editing.

--- bot.py
+++ bot.py
@@ -1,7 +1,24 @@
+#!/usr/bin/env python3
+
 import os, sys
 import mwclient
+import argparse
 
-username, pwd = open(os.path.expanduser('~/.config/cpdl')).read().split()
 
-cpdl = mwclient.Site(('http', 'cpdl.org'), path='/wiki/')
-cpdl.login(username, pwd)
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('page')
+    args = parser.parse_args()
+
+    username, pwd = open(os.path.expanduser('~/.config/cpdl')).read().split()
+
+    cpdl = mwclient.Site(('http', 'cpdl.org'), path='/wiki/')
+    cpdl.login(username, pwd)
+
+    page = cpdl.pages[args.page]
+    text = page.text() + '\nEdited by PreviewBot}.'
+    page.save(text, 'Test editing.')
+
+
+if __name__ == '__main__':
+    main()
